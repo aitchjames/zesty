ac7a1a4e25cf20f06a4ec7c3167401798ca9ca8a
Hayden <hayden@aitch.co>
2020-07-27 14:30:33 +1000

    website optimizations round 1

--- app.js
+++ app.js
@@ -65,7 +65,17 @@ app.use(
     })
 );
 
-app.use(compression());
+app.use(compression({ filter: shouldCompress }))
+ 
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+ 
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
 
 // ROUTES
 app.use('/', viewRouter);
